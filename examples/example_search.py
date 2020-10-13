2b981a8e63c9d249a2d804800ef9bae40f0a3ba3
Hanan <hananfokkens@gmail.com>
2020-10-14 12:57:37 +1300

    fixed example_search.py

--- examples/example_search.py
+++ examples/example_search.py
@@ -1,3 +1,5 @@
+import os
+
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 
@@ -7,18 +9,29 @@ app = Flask(__name__)
 app.config.update({
     "SQLALCHEMY_DATABASE_URI": f"sqlite:///{app.root_path}/../resources/test.db",
     "SQLALCHEMY_TRACK_MODIFICATIONS": False,
-    "IMAGE_SEARCH_PATH_PREFIX": "../resources/image_search/"
+    "IMAGE_SEARCH_PATH_PREFIX": "../resources/image_search_example_search/",
 })
 
 db = SQLAlchemy(app)
-image_search = ImageSearch(app, tensorflow=False)
+image_search = ImageSearch(app)
+
+
+class Model(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String, nullable=False)
+
+    images = db.relationship('Image')
 
 
 @image_search.register()
 class Image(db.Model):
     id = db.Column(db.Integer, primary_key=True)
-    url = db.Column(db.String, nullable=False)
-    model_id = db.Column(db.Integer, nullable=False)
+    path_ = db.Column('path', db.String, nullable=False)
+    model_id = db.Column(db.Integer, db.ForeignKey("model.id"), nullable=False)
+
+    @property
+    def path(self):
+        return os.path.join('../resources/', self.path_)
 
 
 image_search.index_model(Image)
