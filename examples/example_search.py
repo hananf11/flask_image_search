4c69aab45c7f2b69f703750fc8512811ef8859c5
Hanan <hananfokkens@gmail.com>
2020-10-16 13:38:08 +1300

    added new example

--- examples/example_search.py
+++ examples/example_search.py
@@ -5,7 +5,7 @@ from flask_image_search import ImageSearch
 
 app = Flask(__name__)
 app.config.update({
-    "SQLALCHEMY_DATABASE_URI": f"sqlite:///{app.root_path}/../resources/test.db",
+    "SQLALCHEMY_DATABASE_URI": f"sqlite:///{app.root_path}/test.db",
     "SQLALCHEMY_TRACK_MODIFICATIONS": False,
 })
 
@@ -17,7 +17,10 @@ class Radio(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String, nullable=False)
 
-    images = db.relationship('Image')
+    images = db.relationship('Image', backref='radio')
+
+    def __repr__(self):
+        return f"<Radio {self.id} {self.name}>"
 
 
 @image_search.register()
@@ -25,6 +28,9 @@ class Image(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     path = db.Column(db.String, nullable=False)
     radio_id = db.Column(db.Integer, db.ForeignKey("radio.id"), nullable=False)
+
+    def __repr__(self):
+        return f"<Image {self.id} {self.radio.name}>"
 
 
 image_search.index_model(Image)  # index the model so it can be searched
@@ -34,11 +40,11 @@ images = Image.query.with_transformation(image_search.query_search("test.jpg")).
 print(images)
 
 # search using query.image_search
-images = Image.query.image_search("../resources/test.jpg").all()
+images = Image.query.image_search("test.jpg").all()
 print(images)
 
 # join search using query.image_search
-query = Radio.query.join(Image).options(db.contains_eager(Radio.images))
-query = query.image_search("../resources/test.jpg", join=True)
+query = Radio.query.join(Radio.images).options(db.contains_eager(Radio.images))
+query = query.image_search("test.jpg", join=True)
 radios = query.all()
 print(radios)
