8ecf962ff52cb33461d893d4f96851043dd227d4
Hanan F <hananfokkens@gmail.com>
2021-05-03 23:54:14 +1200

    improved example app
    example app now shows distance

--- examples/example_app.py
+++ examples/example_app.py
@@ -29,6 +29,8 @@ class Image(db.Model):
     path = db.Column(db.String, nullable=False)
     radio_id = db.Column(db.Integer, db.ForeignKey("radio.id"), nullable=False)
 
+    distance = db.query_expression()
+
 
 image_search.index_model(Image)
 
@@ -43,9 +45,12 @@ def home():
             flash("Failed to get image")
             return redirect("/")
         image = PILImage.open(f)
-        images = Image.query.order_by(image_search.case(image, Image))
-        radios = Radio.query.join(Radio.images).options(db.joinedload(Radio.images)) \
-            .order_by(image_search.case(image, Image)).all()
+        case = image_search.case(image, Image).label('d')
+        images = Image.query.options(db.with_expression(Image.distance, case)).order_by('d')
+
+        radios = Radio.query.join(Radio.images).options(
+            db.contains_eager(Radio.images).with_expression(Image.distance, case)
+        ).order_by('d')
 
     return render_template_string("""
     {{ get_flashed_messages()[0] }}
